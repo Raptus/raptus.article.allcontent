b3411878b3a39aac8e74f54900e7e08f2a8519b2
Jonas Wagner <jwagner@raptus.com>
2012-11-02 03:04:00 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0a1'
 setup(name='raptus.article.allcontent',
       version=version,
       description="Provides a component which displays the whole content of the contained articles",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='raptus.article.allcontent',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.article/raptus.article.allcontent',
+      url='https://github.com/Raptus/raptus.article.allcontent',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus', 'raptus.article'],
